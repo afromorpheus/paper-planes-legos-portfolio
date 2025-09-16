56a1531e9d12dcdef54f8463c83adbfcfb5b900b
afromorpheus <kabiru@nubianvr.com>
2025-09-16 09:27:36 +0000

    Clean slate: Remove all previous project files for fresh start

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="flex justify-center items-center py-8 bg-white">
-      <p className="text-caption font-ysabeau-sc text-brand-dark-grey">
-        © 2025 KABIRU SEIDU. ALL RIGHTS RESERVED.
-      </p>
-    </footer>
-  )
-}
